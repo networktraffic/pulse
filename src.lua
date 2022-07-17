90036bc7fd4a2accd122b62c8f815518f93f4dd2
network <88440862+networktraffic@users.noreply.github.com>
2022-07-17 10:31:55 -0400

    Update src.lua

--- src.lua
+++ src.lua
@@ -1,1 +1,19 @@
+local pulse = { }
+pulse.__index = pulse
 
+function pulse.new( closure )
+    return setmetatable( { closure = closure }, pulse )
+end
+
+function pulse:set( closure )
+    local backup = hookfunction( self.closure, closure )
+    self.backup = backup
+    
+    return backup
+end
+
+function pulse:undo( )
+    hookfunction( self.closure, self.backup ) 
+end
+
+return pulse
